43b61808895a21ffe6d704ecf4ac65bc77c12615
Debela Mendara <d.mendara5@gmail.com>
2026-09-08 01:08:08 +0300

    final code

--- week01/c#prep03/Program.cs
+++ week01/c#prep03/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+      string KeepPlaying = "yes";
+      while (keepPlaying.ToLower()=="yes"
+    {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int guessCount=0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            
+                Console.WriterLine($"It Tooks You{guessCount}guesses.");
+                Console.WriteLine();
+        }
+          Console.WriteLine("Thanks for playing!");
+        }                    
     }
 }
