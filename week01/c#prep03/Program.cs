ee134cdc9280567f54391eef51c96802d36f2332
Debela Mendara <d.mendara5@gmail.com>
2026-09-09 00:48:36 +0300

    updating the code

--- week01/c#prep03/Program.cs
+++ week01/c#prep03/Program.cs
@@ -4,38 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-      string KeepPlaying = "yes";
-      while (keepPlaying.ToLower()=="yes"
-    {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
+        string keepPlaying = "yes";
 
-        int guess = -1;
-        int guessCount=0;
-
-        while (guess != magicNumber)
+        while (keepPlaying.ToLower() == "yes")
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
-            guessCount++;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-            if (magicNumber > guess)
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
             }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-            
-                Console.WriterLine($"It Tooks You{guessCount}guesses.");
-                Console.WriteLine();
+
+            Console.WriteLine($"It took you {guessCount} guesses.");
+            Console.WriteLine();
+
+            Console.Write("Do you want to play again? ");
+            keepPlaying = Console.ReadLine();
         }
-          Console.WriteLine("Thanks for playing!");
-        }                    
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
